ecebb7b4ef89524358bcfcc6237a9bcc57b7dbb7
All Things Computers <117267670+AllThingsComputers@users.noreply.github.com>
2025-01-22 13:06:46 +0000

    Create http server2.go

---
+++ http server2.go
@@ -0,0 +1,65 @@
+// https://golangdocs.com/golang-docker
+// https://golangdocs.com/http-server-in-golang
+// https://freshman.tech/snippets/go/http-redirect/
+// https://www.programiz.com/golang/variable-scope
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// Global declarations
+var port string = "5555"
+var colonPort = ":" + port
+var consoleMessage = "Listening on port " + port + "...\n" +
+	"Go to http://localhost" + colonPort + "/ in your web browser to access it."
+
+// Handlers
+
+// `welcome` handler for the `/page3` route
+func welcome(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Welcome to the cybersecurity page!")
+}
+
+// `hyperlink` handler for the `/` route
+func hyperlink(w http.ResponseWriter, r *http.Request) {
+	// Responding with information and a simulated hyperlink (as plain text)
+	fmt.Fprintf(w, "Here we can use this HTTP Web Server created in Golang to run simulated threats on the port of your choice!\n")
+	fmt.Fprintf(w, "To access a sample file: C:\\Users\\User.user\\source\\repos\\51. Go series (procmon, ransom, code analyzer, QRX2, scraper, screenshot, slackbot)\\fakedownload.txt\n")
+	fmt.Fprintf(w, "Visit '/page2' or '/page3' for additional pages.")
+}
+
+// `page2` handler
+func page2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is Page 2! Explore other pages for more simulated functionalities.")
+}
+
+// `redirect` handler for a simple HTTP redirect
+func redirect(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/page3", http.StatusFound) // Redirects to `/page3`
+}
+
+// Server configuration using a ServeMux
+func serverMux() {
+	mux := http.NewServeMux()
+
+	// Route handlers
+	mux.HandleFunc("/", hyperlink)
+	mux.HandleFunc("/page2", page2)
+	mux.HandleFunc("/page3", welcome)
+	mux.HandleFunc("/redirect", redirect)
+
+	// Start the server
+	fmt.Println(consoleMessage)
+	if err := http.ListenAndServe(colonPort, mux); err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+}
+
+// Main function
+func main() {
+	serverMux()
+}
